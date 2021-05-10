add5b66224da1cc7fbcb052fa9e919b04874bdb4
nabd0018 <nabd0018@student.monash.edu>
2021-05-10 22:34:59 +0200

    Updating T1-cb-schm.sql (drop and created tables)

--- Assignments/Ass2A/StudentFiles/T1-cb-schm.sql
+++ Assignments/Ass2A/StudentFiles/T1-cb-schm.sql
@@ -19,6 +19,48 @@ Oracle.
 */
 --PLEASE PLACE REQUIRED SQL STATEMENT(S) BELOW
 
+DROP TABLE animal PURGE;
+
+DROP TABLE breeding_event PURGE;
+
+DROP TABLE centre PURGE;
+
+DROP TABLE species PURGE;
+
+CREATE TABLE species (
+    spec_genus              VARCHAR2(20) NOT NULL,
+    spec_name               VARCHAR2(20) NOT NULL,
+    spec_popular_name       VARCHAR2(40) NOT NULL,
+    spec_family             VARCHAR2(20) NOT NULL,
+    spec_natural_range      VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE centre (
+    centre_id               CHAR(6) NOT NULL,
+    centre_name             VARCHAR2(40) NOT NULL,
+    centre_address          VARCHAR2(100) NOT NULL,
+    centre_director         VARCHAR2(30) NOT NULL,
+    centre_phone_no         VARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE breeding_event (
+    brevent_id              NUMBER(6) NOT NULL,
+    brevent_date            DATE NOT NULL,
+    mother_id               NUMBER(6) NOT NULL,
+    father_id               NUMBER(6) NOT NULL
+);
+
+CREATE TABLE animal (
+    animal_id               NUMBER(6) NOT NULL,
+    animal_sex              CHAR(1) NOT NULL,
+    brevent_id              NUMBER(6),
+    centre_id               CHAR(6) NOT NULL,
+    spec_genus              VARCHAR2(20) NOT NULL,
+    spec_name               VARCHAR2(20) NOT NULL
+
+);
+
+
 
 
 
