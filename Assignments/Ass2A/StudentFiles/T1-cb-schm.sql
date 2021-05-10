ad1a1167dff901200d982254e5daa13c3c75a50d
nabd0018 <nabd0018@student.monash.edu>
2021-05-10 22:54:01 +0200

    updating T1-cb-schm.sql (added PK, FK, Unique constraints)

--- Assignments/Ass2A/StudentFiles/T1-cb-schm.sql
+++ Assignments/Ass2A/StudentFiles/T1-cb-schm.sql
@@ -35,6 +35,10 @@ CREATE TABLE species (
     spec_natural_range      VARCHAR2(100) NOT NULL
 );
 
+ALTER TABLE species ADD CONSTRAINT species_pk PRIMARY KEY ( spec_genus,spec_name );
+
+ALTER TABLE species ADD CONSTRAINT uq_spec_popular_name UNIQUE ( spec_popular_name );
+
 CREATE TABLE centre (
     centre_id               CHAR(6) NOT NULL,
     centre_name             VARCHAR2(40) NOT NULL,
@@ -43,12 +47,22 @@ CREATE TABLE centre (
     centre_phone_no         VARCHAR2(20) NOT NULL
 );
 
+ALTER TABLE centre ADD CONSTRAINT centre_pk PRIMARY KEY ( centre_id );
+
+ALTER TABLE centre ADD CONSTRAINT uq_centre_name UNIQUE ( centre_name );
+
 CREATE TABLE breeding_event (
     brevent_id              NUMBER(6) NOT NULL,
     brevent_date            DATE NOT NULL,
     mother_id               NUMBER(6) NOT NULL,
     father_id               NUMBER(6) NOT NULL
 );
+
+ALTER TABLE breeding_event ADD CONSTRAINT breeding_event_pk PRIMARY KEY ( brevent_id );
+
+ALTER TABLE breeding_event ADD CONSTRAINT breeding_event_animal_fk FOREIGN KEY ( mother_id ) REFERENCES animal ( animal_id );
+
+ALTER TABLE breeding_event ADD CONSTRAINT breeding_event_animal_fk FOREIGN KEY ( father_id ) REFERENCES animal ( animal_id );
 
 CREATE TABLE animal (
     animal_id               NUMBER(6) NOT NULL,
@@ -60,7 +74,12 @@ CREATE TABLE animal (
 
 );
 
+ALTER TABLE animal ADD CONSTRAINT animal_pk PRIMARY KEY ( animal_id );
 
+ALTER TABLE animal ADD CONSTRAINT animal_breeding_fk FOREIGN KEY ( brevent_id ) REFERENCES breeding_event ( brevent_id );
 
+ALTER TABLE animal ADD CONSTRAINT animal_centre_fk FOREIGN KEY ( centre_id ) REFERENCES centre ( centre_id );
 
+ALTER TABLE animal ADD CONSTRAINT animal_species_fk FOREIGN KEY ( spec_genus ) REFERENCES species ( spec_genus );
 
+ALTER TABLE animal ADD CONSTRAINT animal_species_fk FOREIGN KEY ( spec_name ) REFERENCES species ( spec_name );
