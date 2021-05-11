f4d75e02ff6e107df90f08c4f8d9ab6a94def5c6
nabd0018 <nabd0018@student.monash.edu>
2021-05-11 20:29:47 +0200

    updating T3-cb-alter.sql

--- Assignments/Ass2A/StudentFiles/T3-cb-alter.sql
+++ Assignments/Ass2A/StudentFiles/T3-cb-alter.sql
@@ -21,14 +21,27 @@ Make the listed changes to the "live" database
 
 -- (a)
 
+ALTER TABLE centre ADD (
+    num_of_offspring_born NUMBER(6) DEFAULT 0
+);
 
+COMMENT ON COLUMN centre.num_of_offspring_born IS
+    'The total number of offspring which are born at each centre';
+    
 -- (b)
 /*
 ******* Explain here your selected approach and its advantage/s *********
-
+I chose to add a new column "deceased" in the animal table, which will either be 'Y' if the animal died or 'N' if the animal is alive.
+The default value is 'N' because all animals are alive to begin with
 */
 
+ALTER TABLE animal ADD (
+    deceased CHAR(1) DEFAULT 'N'
+);
 
+COMMENT ON COLUMN animal.deceased IS
+    'The status of the animal (alive or deceased)';
 
 -- (c)
 
+COMMIT;
