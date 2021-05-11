169a5888e3663e57f10adde970493d32c1fd1b8c
nabd0018 <nabd0018@student.monash.edu>
2021-05-11 22:12:56 +0200

    updating T2-cb-dm.sql & T2-cb-insert.sql

--- Assignments/Ass2A/StudentFiles/T2-cb-dm.sql
+++ Assignments/Ass2A/StudentFiles/T2-cb-dm.sql
@@ -63,7 +63,7 @@ INSERT INTO animal VALUES (
 
 INSERT INTO breeding_event VALUES (
     breeding_event_seq.NEXTVAL,
-    '10-Feb-2021',
+    TO_DATE('2021-02-10', 'YYYY-MM-DD'),
     (SELECT animal_id from animal WHERE centre_id  = (SELECT centre_id FROM centre WHERE centre_name = 'Australia Zoo') AND spec_genus = (SELECT spec_genus FROM species WHERE spec_popular_name = 'Tasmanian Devil')),
     (SELECT animal_id from animal WHERE centre_id  = (SELECT centre_id FROM centre WHERE centre_name = 'Werribee Open Range Zoo') AND spec_genus = (SELECT spec_genus FROM species WHERE spec_popular_name = 'Tasmanian Devil'))
 );
